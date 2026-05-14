d21856bba58bc37cb2dbc0b8b3b9c3734da06d14
Bruh277 <247578952+Bruh277@users.noreply.github.com>
2026-05-14 02:40:56 +0200

    Completed exercise repeatString. All tests passed.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if(num < 0){
+        return "ERROR";
+    }
+
+    let i = 0;
+    let word = "";
+
+    while(i < num){
+        word += string;
+        i++;
+    }
+
+    return word;
 
 };
 

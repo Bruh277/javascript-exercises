4af4cb357a77cc08b9468b2c08d3b4348aa9f6ab
Bruh277 <247578952+Bruh277@users.noreply.github.com>
2026-05-14 03:10:20 +0200

    Completed exercise reverseString. All tests passed.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const word = string;
+
+    let characterArray = word.split('');
+    let reversedArray = characterArray.reverse();
+    let reversedWord = reversedArray.join('');
+
+    return reversedWord;
+    
 };
 
 // Do not edit below this line
